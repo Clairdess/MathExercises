a2b2b97556c233bf968e2a4bb5f43aff0fbb48bc
Daniil Davydov <clairdess.github@gmail.com>
2022-05-05 20:26:09 +0900

    Delete unused imports

--- src/main/java/com/clairdess/physics/ExerciseRepo.java
+++ src/main/java/com/clairdess/physics/ExerciseRepo.java
@@ -1,11 +1,5 @@
 package com.clairdess.physics;
 
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
 //@Repository
 //@EnableJpaRepositories
 //public interface ExerciseRepo extends CrudRepository<Exercise, Long> {
